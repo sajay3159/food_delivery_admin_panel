34c1bc20a3c51a63190928ae5f478431a781e07f
Ajay Sahani <ajay.sahani@gmail.com>
2025-02-07 16:53:27 +0530

    change backend url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url = "https://food-delivery-backend-k38k.onrender.com";
+  const url = "https://food-delivery-backend-8tnl.onrender.com";
 
   return (
     <div>
