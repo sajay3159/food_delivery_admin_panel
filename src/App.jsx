683f0630e20812f2c50a31899dc915574b40c3f0
Ajay Sahani <ajay.sahani@gmail.com>
2025-02-11 09:29:05 +0530

    change url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url = "https://food-delivery-backend-8tnl.onrender.com";
+  const url = "http://localhost:4000";
 
   return (
     <div>
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,13 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const assets ={
+
+
+export const url = 'http://localhost:4000'
+export const currency = '₹'
+
+
+export const assets = {
     logo,
     add_icon,
     order_icon,
@@ -14,4 +20,3 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
