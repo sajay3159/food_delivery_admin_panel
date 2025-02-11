738efa0eda39e373082c80f3294f3d6db2fd2214
Ajay Sahani <115401286+sajay3159@users.noreply.github.com>
2025-02-11 09:41:41 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,7 +7,7 @@ import parcel_icon from './parcel_icon.png'
 
 
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-delivery-backend-hc0l.onrender.com'
 export const currency = '₹'
 
 
